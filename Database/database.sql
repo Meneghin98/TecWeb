17f97d8008a25ba8fede0f7979edd5ee78bc36aa
TintoEdoardo <tinto.edoardo@gmail.com>
2019-10-21 16:49:33 +0200

    Terminato il database

--- Database/database.sql
+++ Database/database.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS Categories;
 CREATE TABLE Users
 (
     nickname VARCHAR (50) PRIMARY KEY,
-    password VARCHAR (50) NOT NULL,
+    pwd VARCHAR (50) NOT NULL,
     email VARCHAR (50) NOT NULL,
     username VARCHAR (50) NOT NULL,
     surname VARCHAR (50) NOT NULL,
@@ -16,12 +16,25 @@ CREATE TABLE Users
     ref VARCHAR (50)
 );
 
+CREATE TABLE Articles
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    link VARCHAR (50) NOT NULL,
+    creation_date TIMESTAMP DEFAULT(CURRENT_TIMESTAMP()),
+    title VARCHAR(50) NOT NULL,
+    article_type VARCHAR(50) NOT NULL,
+    views INT(10) NOT NULL,
+    category INT (5) REFERENCES Categories (id),
+    editor VARCHAR(50) REFERENCES Users(nickname) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 CREATE TABLE Comments
 (
     id INT (5) PRIMARY KEY AUTO_INCREMENT,
     creation_date CHAR (32) NOT NULL,
-    text VARCHAR (296) NOT NULL,
-    article INT (5) REFERENCES Articles (id)
+    txt VARCHAR (296) NOT NULL,
+    user VARCHAR(50) REFERENCES Users(nickname) ON DELETE CASCADE ON UPDATE CASCADE,
+    article INT (5) REFERENCES Articles (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Likes
@@ -30,31 +43,29 @@ CREATE TABLE Likes
     id INT (5) NOT NULL,
 
     PRIMARY KEY (nickname, id),
-    FOREIGN KEY (nickname) REFERENCES Users(nickname),
-    FOREIGN KEY (id) REFERENCES Comments (id)
-
-);
-
-CREATE TABLE Articles
-(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    link VARCHAR (50) NOT NULL,
-    creation_date TIMESTAMP DEFAULT CURRENT_DATE(),
-    title VARCHAR(50) NOT NULL,
-    article_type VARCHAR(50) NOT NULL,
-    views INT(10) NOT NULL,
-    category INT (5) REFERENCES Categories (id)
+    FOREIGN KEY (nickname) REFERENCES Users(nickname) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id) REFERENCES Comments (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Images
 (
-    link VARCHAR (50) PRIMARY KEY
-    article INT (5) REFERENCES Articles (id)
-    alt VARCHAR (10) NOT NULL
+    link VARCHAR (50) PRIMARY KEY,
+    alt VARCHAR (10) NOT NULL,
+    article INT (5) REFERENCES Articles (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Categories
 (
-    id INT (5) PRIMARY KEY
+    id INT (5) PRIMARY KEY,
     names VARCHAR (50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Follows
+(
+    nickname VARCHAR(50) NOT NULL,
+    id INT (5) NOT NULL,
+
+    PRIMARY KEY (nickname, id),
+    FOREIGN KEY (nickname) REFERENCES Users(nickname) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id) REFERENCES Categories(id) ON DELETE CASCADE ON UPDATE CASCADE
 );

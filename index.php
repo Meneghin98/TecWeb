2831314a13aec315603267d35c408705e979a63f
Simone Meneghin <simone.meneghin.98@outlook.it>
2019-12-13 13:29:06 +0100

    Corretto path immagini

--- index.php
+++ index.php
@@ -13,7 +13,9 @@ if (!is_null($queryArticoli)) {
         $articolo = $queryArticoli[$i];
         $top_articoli .= "<li><a href=\"$articolo[path]\"><img src=\"$articolo[img]\" alt=\"$articolo[alt]\"/><span class=\"topArticleTitle\">$articolo[titolo]</span></a></li>";
     }
-    $index = str_replace("£Articoli", html::articoli($queryArticoli, $i), $index);
+    $articoli = html::articoli($queryArticoli, $i);
+    $articoli = str_replace('../', '', $articoli);
+    $index = str_replace("£Articoli", $articoli, $index);
 }
 $db->close();
 echo str_replace('£top_articles', $top_articoli, $index);

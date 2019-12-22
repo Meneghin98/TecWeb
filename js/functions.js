bc17bf1d0258a21e8cf6539e5861fea70b574dca
Simone Meneghin <simone.meneghin.98@outlook.it>
2019-12-22 17:03:00 +0100

    Aggiunta funzione conteggio lettere

--- js/functions.js
+++ js/functions.js
@@ -1,64 +1,52 @@
+function trimText(id) {
+    let element = document.getElementById(id);
+    element.innerHTML = element.textContent.trim();
+}
 
 
-
+function updateNum() {//funziona su edge ma non su chrome, WTF?!?!?!?
+    let numChar = document.getElementById("textarea").textContent.length;
+    document.getElementById("MaxChar").innerHTML = (300 - numChar).toString();
+}
 
 function miPiaceOver(id) {
     let classList = document.getElementById(id).classList;
-    if(classList.contains("upBlue"))
+    if (classList.contains("upBlue"))
         return;
     if (classList.contains("upGray")) {
         classList.remove("upGray");
         classList.add("upBlack");
     }
 }
+
 function miPiaceOut(id) {
     let classList = document.getElementById(id).classList;
-    if(classList.contains("upBlue"))
+    if (classList.contains("upBlue"))
         return;
-    if (classList.contains("upBlack")){
+    if (classList.contains("upBlack")) {
         classList.remove("upBlack");
         classList.add("upGray")
     }
 }
+
 function miPiace(id) {
     let classList = document.getElementById(id).classList;
-    if (classList.contains("upBlue")){
+    if (classList.contains("upBlue")) {
         classList.remove("upBlue");
         classList.add("upBlack");
 
         $.ajax({
             type: "GET",
-            url: "like.php?add=false&id="+id,
-            success: function(response)
-            {
-                console.log('dati ricevuti : '+response);
-
-                // qui viene gestita la risposta
-            },
-            error: function(errore)
-            {
-                alert('ERRORE : il server non risponde o lo ha fatto in modo anomalo '+errore);
-            }
+            url: "like.php?add=false&id=" + id
         })
 
-    }
-    else{
+    } else {
         classList.remove("upBlack");
         classList.add("upBlue");
 
         $.ajax({
             type: "GET",
-            url: "like.php?add=true&id="+id,
-            success: function(response)
-            {
-                console.log('dati ricevuti : '+response);
-
-                // qui viene gestita la risposta
-            },
-            error: function(errore)
-            {
-                alert('ERRORE : il server non risponde o lo ha fatto in modo anomalo '+errore);
-            }
+            url: "like.php?add=true&id=" + id
         })
     }
 }

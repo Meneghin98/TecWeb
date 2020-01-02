ccb0ed9e66c9818cd0afbf4474684b68e6578c11
EnricoSalmaso <enrico.salmaso@yahoo.it>
2020-01-02 19:01:08 +0100

    aggiunti una new e una guida

--- Database/database.sql
+++ Database/database.sql
@@ -1,4 +1,5 @@
-create DATABASE IF NOT EXISTS DecryptedGames;
+create DATABASE
+IF NOT EXISTS DecryptedGames;
 
 drop table IF EXISTS images;
 drop table IF EXISTS follows;
@@ -13,54 +14,85 @@ drop procedure IF EXISTS IncrementaVisualizzazioni;
 create TABLE users
 (
     nickname VARCHAR(50) PRIMARY KEY,
-    pwd      VARCHAR(50) NOT NULL,
-    email    VARCHAR(50) NOT NULL,
+    pwd VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL,
-    surname  VARCHAR(50) NOT NULL,
-    usertype VARCHAR(5)  NOT NULL,
-    ref      VARCHAR(50),
-    img_src  VARCHAR(50) default 'default.jpg'
+    surname VARCHAR(50) NOT NULL,
+    usertype VARCHAR(5) NOT NULL,
+    ref VARCHAR(50),
+    img_src VARCHAR(50) default 'default.jpg'
 );
 
 create TABLE categories
 (
-    id    INT(5) PRIMARY KEY AUTO_INCREMENT,
-    names VARCHAR(50) UNIQUE NOT NULL
+    id INT(5) PRIMARY KEY AUTO_INCREMENT,
+    names VARCHAR
+(50) UNIQUE NOT NULL
 );
 
 create TABLE articles
 (
-    id             INT PRIMARY KEY AUTO_INCREMENT,
-    path           VARCHAR(50)  NOT NULL,
-    creation_date  DATE DEFAULT (CURRENT_TIMESTAMP()),
-    title          VARCHAR(300) NOT NULL,
-    description    VARCHAR(300),
-    category_title VARCHAR(50)  NOT NULL,
-    article_type   VARCHAR(50)  NOT NULL,
-    views          INT(10)      NOT NULL,
-    category       INT(5),
-    editor         VARCHAR(50),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    path           VARCHAR
+(50)  NOT NULL,
+    creation_date  DATE DEFAULT
+(CURRENT_TIMESTAMP
+()),
+    title          VARCHAR
+(300) NOT NULL,
+    description    VARCHAR
+(300),
+    category_title VARCHAR
+(50)  NOT NULL,
+    article_type   VARCHAR
+(50)  NOT NULL,
+    views          INT
+(10)      NOT NULL,
+    category       INT
+(5),
+    editor         VARCHAR
+(50),
 
-    CONSTRAINT FK_editor FOREIGN KEY (editor) REFERENCES users (nickname) ON delete CASCADE ON update CASCADE,
-    CONSTRAINT FK_category_art FOREIGN KEY (category) REFERENCES categories (id) ON delete CASCADE ON update CASCADE
+    CONSTRAINT FK_editor FOREIGN KEY
+(editor) REFERENCES users
+(nickname) ON
+delete CASCADE ON
+update CASCADE,
+    CONSTRAINT FK_category_art FOREIGN KEY
+(category) REFERENCES categories
+(id) ON
+delete CASCADE ON
+update CASCADE
 );
 
 create TABLE comments
 (
-    id            INT(5) PRIMARY KEY AUTO_INCREMENT,
-    creation_date DATETIME DEFAULT CURRENT_TIMESTAMP(),
-    txt           VARCHAR(300) NOT NULL,
-    user          VARCHAR(50),
-    article       INT(5),
+    id INT(5) PRIMARY KEY AUTO_INCREMENT,
+    creation_date DATETIME DEFAULT CURRENT_TIMESTAMP
+(),
+    txt           VARCHAR
+(300) NOT NULL,
+    user          VARCHAR
+(50),
+    article       INT
+(5),
 
-    CONSTRAINT FK_user FOREIGN KEY (user) REFERENCES users (nickname) ON delete CASCADE ON update CASCADE,
-    CONSTRAINT FK_article FOREIGN KEY (article) REFERENCES articles (id) ON delete CASCADE ON update CASCADE
+    CONSTRAINT FK_user FOREIGN KEY
+(user) REFERENCES users
+(nickname) ON
+delete CASCADE ON
+update CASCADE,
+    CONSTRAINT FK_article FOREIGN KEY
+(article) REFERENCES articles
+(id) ON
+delete CASCADE ON
+update CASCADE
 );
 
 create TABLE likes
 (
     nickname VARCHAR(50) NOT NULL,
-    id       INT(5)      NOT NULL,
+    id INT(5) NOT NULL,
 
     PRIMARY KEY (nickname, id),
     CONSTRAINT FK_nick_like FOREIGN KEY (nickname) REFERENCES users (nickname) ON delete CASCADE ON update CASCADE,
@@ -69,8 +101,8 @@ create TABLE likes
 
 create TABLE images
 (
-    src     VARCHAR(50) PRIMARY KEY,
-    alt     VARCHAR(10) NOT NULL,
+    src VARCHAR(50) PRIMARY KEY,
+    alt VARCHAR(10) NOT NULL,
     article INT(5),
     CONSTRAINT FK_art_img FOREIGN KEY (article) REFERENCES articles (id) ON delete CASCADE ON update CASCADE
 );
@@ -79,115 +111,185 @@ DELIMITER ]
 CREATE PROCEDURE IncrementaVisualizzazioni(_id int)
 BEGIN
 
-    UPDATE articles a  SET a.views = a.views+1 WHERE a.id = _id;
+    UPDATE articles a
+    SET a
+    .views = a.views+1 WHERE a.id = _id;
 
 END ]
 DELIMITER ;
 
 insert into users
     (nickname, pwd, email, username, surname, usertype, ref)
-values ('admin', 'admin', 'admin@admin.it', 'admin', 'admin', 'admin', null);
+values
+    ('admin', 'admin', 'admin@admin.it', 'admin', 'admin', 'admin', null);
 insert into users
     (nickname, pwd, email, username, surname, usertype, ref)
-values ('user', 'user', 'user@user.it', 'user', 'user', 'user', null);
+values
+    ('user', 'user', 'user@user.it', 'user', 'user', 'user', null);
 insert into users
-(nickname, pwd, email, username, surname, usertype, ref)
-values ('Simone', 'user', 'simone.meneghin@studenti.unipd.it', 'Simone', 'Meneghin', 'admin', null);
+    (nickname, pwd, email, username, surname, usertype, ref)
+values
+    ('Simone', 'user', 'simone.meneghin@studenti.unipd.it', 'Simone', 'Meneghin', 'admin', null);
 
 
 
 
 insert into categories
     (id, names)
-values (null, 'categoriaHardware'); /*1*/
+values
+    (null, 'categoriaHardware');
+/*1*/
 insert into categories
     (id, names)
-values (null, 'categoriaEventi');/*2*/
+values
+    (null, 'categoriaEventi');/*2*/
 insert into categories
     (id, names)
-values (null, 'categoriaConsole');/*3*/
+values
+    (null, 'categoriaConsole');/*3*/
 insert into categories
     (id, names)
-values (null, 'categoriaVideogiochi');/*4*/
+values
+    (null, 'categoriaVideogiochi');/*4*/
 insert into categories
     (id, names)
-values (null, 'categoriaAltro');/*5*/
+values
+    (null, 'categoriaAltro');/*5*/
 
 
-insert into articles /*Lucca comics*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/News/LuccaRecord.html', '2019-11-03', 'Lucca Comics. Oltre 88mila biglietti: è record assoluto',
+insert into articles
+    /*Lucca comics*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/News/LuccaRecord.html', '2019-11-03', 'Lucca Comics. Oltre 88mila biglietti: è record assoluto',
         'Nella giornata di ieri, 02/11/19, nonostante il maltempo, è stato record di biglietti per LuccaC&amp;G 2019',
         'Lucca C&amp;G 19', 'News', '0', '2', null);
-insert into articles /*PokemonGO Guida*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/Altro/guidaPokemonGo.html', '2019-12-30','Pokémon GO, come sconfiggere Sierra del Team Rocketcon un solo Pokémon',
+insert into articles
+    /*Whatsapp*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/News/WhatsappNews.html', '2020-01-01', 'Whatsapp: il virus di Capodanno con un messaggio ruba i dati dai telefoni iOs e Android',
+        'Whatsapp: il virus di Capodanno si nasconde in un semplice messaggio, ma dopo un click ruba i dati dai telefoni iOS e Android.',
+        'Whatsapp', 'News', '0', '5', null);
+insert into articles
+    /*Fortnite Guida*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/Altro/guidaFortnite.html', '2020-01-02', 'Fortnite: come ottenere il deltaplano 2020 gratis ',
+        'Epic Games sta regalando un deltaplano 2020 gratis a tutti i giocatori, per festeggiare l''anno nuovo: ecco come ottenerlo facilmente.',
+        'Fortnite', 'Altro', '0', '4', null);
+insert into articles
+    /*PokemonGO Guida*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/Altro/guidaPokemonGo.html', '2019-12-30', 'Pokémon GO, come sconfiggere Sierra del Team Rocketcon un solo Pokémon',
         'Questo video vi mostra come sconfiggere Sierra del Team GO Rocket di Pokémon GO utilizzando uno ed un solo Pokémon, ecco la guida.',
-        'Pokemon Go', 'Altro', '0', '3', null);
-insert into articles /*Dead Stranding Recensione*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/Recensione/recensioneDeadStranding.html', '2019-12-04','Dead Stranding, la recensione - PS4',
+        'Pokemon Go', 'Altro', '0', '4', null);
+insert into articles
+    /*Dead Stranding Recensione*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/Recensione/recensioneDeadStranding.html', '2019-12-04', 'Dead Stranding, la recensione - PS4',
         'La recensione di Death Stranding: dopo tre anni di misteri e domande arriva su PS4 l''ultima opera, ma anche un po'' la rinascita di Hideo Kojima. È davvero rivoluzione?',
         'Dead Stranding - recensione', 'Recensione', '0', '4', null);
-insert into articles/*Vampyr*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/Recensioni/recensioneVampyr.html', '2018-06-05', 'Vampyr: la recensione',
+insert into articles
+    /*Vampyr*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/Recensioni/recensioneVampyr.html', '2018-06-05', 'Vampyr: la recensione',
         'L''atteso action RPG di <span xml:lang="en">Dontnod Entertainment</span> ci porta nella Londra del 1918, fra vampiri ed epidemie',
         'Vampyr - recensione', 'Recensioni', '0', '4', null);
-insert into articles/*Cyberpunk*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/News/Cyberpunk-2077.html', '2019-11-28',
+insert into articles
+    /*Cyberpunk*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/News/Cyberpunk-2077.html', '2019-11-28',
         '<span xml:lang="en">Cyberpunk 2077 "You are breathtaking!"</span>',
         'Dopo che <span xml:lang="en">Keanu Reevs</span> è salito sul palco dell''<span xml:lang="en">E3</span> 2019 per la presentazione di <span xml:lang="en">Cyberpunk 2077</span>, "<span xml:lang="en">You''re Breathtaking</span>" è il tormentone del momento',
         '<span xml:lang="en">Cyberpunk 2077</span>', 'News', '0', '4', null);
-insert into articles/*Cod*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/Recensioni/Call-of-duty-MW-2019.html', '2019-09-25', 'Recensione: <span xml:lang="en">Call of Duty: Modern Warfare</span>',
+insert into articles
+    /*Cod*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/Recensioni/Call-of-duty-MW-2019.html', '2019-09-25', 'Recensione: <span xml:lang="en">Call of Duty: Modern Warfare</span>',
         'ritorno al passato certo, ma soprattutto un nuovo, grande inizio',
         'Call of Duty MW', 'Recensioni', '0', '4', null);
-insert into articles/*monitor*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/News/Xiaomimonitor.html', '2019-10-18', 'Xiaomi entra nel mondo dei monitor con<span xml:lang="en"> Mi Surface Display</span>, un 34 pollici <span xml:lang="en"><abbr title="Wide Quad HD">WQHD</abbr></span> con <span xml:lang="en">FreeSync</span>',
+insert into articles
+    /*monitor*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/News/Xiaomimonitor.html', '2019-10-18', 'Xiaomi entra nel mondo dei monitor con<span xml:lang="en"> Mi Surface Display</span>, un 34 pollici <span xml:lang="en"><abbr title="Wide Quad HD">WQHD</abbr></span> con <span xml:lang="en">FreeSync</span>',
         '',
         'Xiaomi Monitor', 'News', '0', '1', null);
-insert into articles/*star wars*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/Recensioni/recensioneStarWars.html', '2019-11-16', '<span xml:lang="en">Star Wars Jedi: Fallen Order</span>, la recensione - <span xml:lang="en"><abbr title="Play Station 4">PS4</abbr></span>',
+insert into articles
+    /*star wars*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/Recensioni/recensioneStarWars.html', '2019-11-16', '<span xml:lang="en">Star Wars Jedi: Fallen Order</span>, la recensione - <span xml:lang="en"><abbr title="Play Station 4">PS4</abbr></span>',
         '',
         'Star Wars - PS4', 'Recensioni', '0', '4', null);
-insert into articles/*death stranding*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/News/DeathStrandingPC.html', '2019-10-30', '<span xml:lang="en "> Death Stranding</span>: versione PC e periodo di uscita confermati',
+insert into articles
+    /*death stranding*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/News/DeathStrandingPC.html', '2019-10-30', '<span xml:lang="en "> Death Stranding</span>: versione PC e periodo di uscita confermati',
         '',
         'Death Stranding PC', 'News', '0', '4', null);
-insert into articles/*StarWars vendite*/
-(id, path, creation_date, title, description, category_title, article_type, views, category, editor)
-values (null, 'html/News/StarWarsPCSell.html', '2019-11-27', '<span xml:lang="en">Star Wars Jedi: Fallen Order</span>, versione PC oltre le aspettative, battuti diversi record', 'Vendite PC, battuti molti record','Star Wars vendite PC','News', '0', '4', null);
+insert into articles
+    /*StarWars vendite*/
+    (id, path, creation_date, title, description, category_title, article_type, views, category, editor)
+values
+    (null, 'html/News/StarWarsPCSell.html', '2019-11-27', '<span xml:lang="en">Star Wars Jedi: Fallen Order</span>, versione PC oltre le aspettative, battuti diversi record', 'Vendite PC, battuti molti record', 'Star Wars vendite PC', 'News', '0', '4', null);
 
 
 
 
 insert into images
     (src, alt, article)
-values ('images/Lucca-Comics-Games-982x540.jpg', '', '1');
+values
+    ('images/Lucca-Comics-Games-982x540.jpg', '', '1');
+
 insert into images
     (src, alt, article)
-values ('images/Recensioni/vampyr/vampyr.jpg', '', '2');
+values
+    ('images/News/whatsapp.jpg', '', '2');
 insert into images
     (src, alt, article)
-values ('images/News/Cyberpunk/CP20771-lowRes.jpg', '', '3');
+values
+    ('images/guide/fortniteDeltaplano.jpg', '', '3');
 insert into images
-(src, alt, article)
-values ('images/Recensioni/cod-mw-2019/codmw.png', '', '4');
+    (src, alt, article)
+values
+    ('images/guide/sierra.jpg', '', '4');
 insert into images
-(src, alt, article)
-values ('images/mi-surface-display.jpg', '', '5');
+    (src, alt, article)
+values
+    ('images/Recensioni/dead-stranding/img0.jpg', '', '5');
 insert into images
-(src, alt, article)
-values ('images/Recensioni/Star-Wars/starwars.png', '', '6');
+    (src, alt, article)
+values
+    ('images/Recensioni/vampyr/vampyr.jpg', '', '6');
 insert into images
-(src, alt, article)
-values ('images/death-stranding-image.jpg', '', '7');
+    (src, alt, article)
+values
+    ('images/News/Cyberpunk/CP20771-lowRes.jpg', '', '7');
 insert into images
-(src, alt, article)
-values ('images/News/StarWars/StarWarsNews.jpg', '', '8');
+    (src, alt, article)
+values
+    ('images/Recensioni/cod-mw-2019/codmw.png', '', '8');
+insert into images
+    (src, alt, article)
+values
+    ('images/mi-surface-display.jpg', '', '9');
+insert into images
+    (src, alt, article)
+values
+    ('images/Recensioni/Star-Wars/starwars.png', '', '10');
+insert into images
+    (src, alt, article)
+values
+    ('images/death-stranding-image.jpg', '', '11');
+insert into images
+    (src, alt, article)
+values
+    ('images/News/StarWars/StarWarsNews.jpg', '', '12');
--- php/login.php
+++ php/login.php
@@ -9,7 +9,7 @@ $file = str_replace('£head_', html::head(), $file);
 $file = str_replace('£footer', html::footer(), $file);
 $file = str_replace('£header', html::header(), $file);
 
-if(isset($_POST['Accedi'])) {
+if (isset($_POST['Accedi'])) {
 
     $db = new DBConnection();
 
@@ -23,36 +23,46 @@ if(isset($_POST['Accedi'])) {
     if (!$db->exitsPassword($password))
         $errori .= "<li>La password inserita è errata</li>";
 
-    if($errori!="") {
-        $beginList = '<ul>'.$errori.'</ul>';
-        $file = str_replace('£erroriLogin', $beginList, $file );
-    }
-    else {
-        $nickname = "SELECT 'nickname' FROM 'users' WHERE 'email'=$email AND 'password'=$password";
-
-        $db->close();
-
-        $_SESSION['loggato']=true;
-        $_SESSION["nickname"]=$nickname;
-        header("Location: ../index.php");
-    }
-
-    echo $file;
-
-}
-else { //è la prima volta che accedo
-    $form = '<div class="emailLog">
+    if ($errori != "") {
+        $form = '<div class="emailLog">
                     <img src="../../images/icons/email1.png" alt="icona email">
                     <label for="emailLogin" xml:lang="en"><span xml:lang="en">E-mail</span>:</label>
-                    <input type="text" name="emailLogin" id="emailLogin" maxlength="30" />
+                    <input type="text" name="emailLogin" id="emailLogin" value=\'$_POST["emailLogin"]\' />
              </div>
              <div class="passwordLog">
                     <img src="../../images/icons/lucchetto1.png" alt="icona password">
                     <label for="passwordLogin" xml:lang="en"><span xml:lang="en">Password</span>:</label>
-                    <input type="password" name="password" id="passwordLogin" maxlength="20" />
+                    <input type="password" name="password" id="passwordLogin" value=\'$password\' />
+             </div>';
+        $file = str_replace('£form', $form, $file);
+        $file = str_replace('£erroriLogin', "", $file);
+        $beginList = '<ul>' . $errori . '</ul>';
+        $file = str_replace('£erroriLogin', $beginList, $file);
+        echo $file;
+    } else {
+        $nickname = "SELECT 'nickname' FROM 'users' WHERE 'email'=$email AND 'password'=$password";
+
+        $db->close();
+
+        $_SESSION['loggato'] = true;
+        $_SESSION["nickname"] = $nickname;
+        header("Location: ../index.php");
+    }
+
+
+} else { //è la prima volta che accedo
+    $form = '<div class="emailLog">
+                    <img src="../../images/icons/email1.png" alt="icona email">
+                    <label for="emailLogin" xml:lang="en"><span xml:lang="en">E-mail</span>:</label>
+                    <input type="text" name="emailLogin" id="emailLogin" value=""/>
+             </div>
+             <div class="passwordLog">
+                    <img src="../../images/icons/lucchetto1.png" alt="icona password">
+                    <label for="passwordLogin" xml:lang="en"><span xml:lang="en">Password</span>:</label>
+                    <input type="password" name="password" id="passwordLogin" value=""/>
              </div>';
     $file = str_replace('£form', $form, $file);
-    $file = str_replace('£erroriLogin',"", $file);
+    $file = str_replace('£erroriLogin', "", $file);
 
     echo $file;
 

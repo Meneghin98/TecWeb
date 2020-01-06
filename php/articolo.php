b52dd48dbb4ca5bd1c01fc5105e731e67487fad8
TintoEdoardo <tinto.edoardo@gmail.com>
2020-01-06 19:17:38 +0100

    Rimosso sezione superflua dello script

--- php/articolo.php
+++ php/articolo.php
@@ -24,9 +24,10 @@
     $paginaArticolo = str_replace('£footer', html::linked_obj('footer', 'article'), $paginaArticolo);
     $paginaArticolo = str_replace('£menu_', html::linked_obj('menu', 'article'), $paginaArticolo);
 
+    /* Path all'articolo corrente
     $_string = $_SERVER['REQUEST_URI'];
     $_string = $_SERVER['SCRIPT_FILENAME'].$_string;
-    $paginaArticolo = str_replace('£current_URI_ ', '/php/articolo.php?'.$_SERVER['QUERY_STRING'], $paginaArticolo);
+    $paginaArticolo = str_replace('£current_URI_ ', '/php/articolo.php?'.$_SERVER['QUERY_STRING'], $paginaArticolo); */
 
     $likes = null;
     if ($_SESSION['loggato'] == true) // utente loggato

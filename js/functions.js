d4bb6baa2409f068b50cb5d1571b3da070091a53
EnricoSalmaso <enrico.salmaso@yahoo.it>
2020-01-09 13:35:25 +0100

    aggiunta a function.js la parte login (ancora da implementare)

--- js/functions.js
+++ js/functions.js
@@ -58,3 +58,87 @@ function miPiace(id) {
         })
     }
 }
+
+// ------------------------- LOGIN --------------------------
+
+
+function mostraErrore(input,testoErrore) {
+
+    togliErrore(input);
+
+    var p = input.parentNode;
+    if(p.children.length == 2) {
+        var strong = document.createElement("strong");
+        //strong.className="corsivo";
+        strong.appendChild(document.createTextElement(testoErrore)); //span.innerHTML=testoErrore;
+        p.appendChild
+        p.appendChild(strong);
+    }
+}
+
+function togliErrore(input) {
+    var p = input.parentNode;
+
+    if(p.children.length > 2) {
+        p.removeChild(p.children[2]); //rimuovo il terzo figlio di p
+    }
+
+    /*var span = p.lastChild;
+    p.removeChild(span);*/
+}
+
+
+function checkNome(Nomeinput) {
+    var patt = new RegExp('^[a-zA-Z]{3,}$');
+    if(patt.tets(Nomeinput.value)) {  //mi mostra il valore contenuto in input
+        togliErrore(Nomeinput);
+        return true;
+    }
+    else {
+        mostraErrore(Nomeinput,
+            "Nome inserito non corretto (almeno 3 lettere) ");
+        return false;
+    }
+}
+
+function checkColore() {
+    return false;
+}
+
+function checkPeso() {
+    var patt = new RegExp('^[1-9][0-9]{0,2}')
+    return false;
+}
+
+function checkDescrizione() {
+    return false;
+}
+
+
+function validazioneForm() {
+    var nome = document.getElementById("nome");
+    var colore = document.getElementById("colore");
+    var peso = document.getElementById("peso");
+    var descrizione = document.getElementById("descrizione");
+
+    var risultatoNome = checkNome();
+    var risultatoColore = checkColore();
+    var risultatoPeso = checkPeso();
+    var risultatoDescrizione = checkDescrizione();
+
+    return risultatoNome && risultatoColore &&  risultatoPeso && risultatoDescrizione;
+
+}
+
+/*
+<form action="insert.php" onsubmit="return validazioneForm()">
+    <p>
+        <label>
+        <input id="nome">
+        <span>Errore nellínserimento del nome</span>
+
+*/
+
+// --------------------- FINE LOGIN ---------------------------
+
+// ------------------- REGIS

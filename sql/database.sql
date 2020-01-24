a2d08aa6fc763fce386700d7f1b33a999efeb198
Enrico <enricosalmaso97@outlook.it>
2020-01-24 11:44:59 +0100

    LOGIN e CHI SIAMO
    aggiunto account e aggiunto title nell'immagine del login

--- sql/database.sql
+++ sql/database.sql
@@ -99,6 +99,9 @@ values ('Simone', 'admin', 'simone.meneghin@studenti.unipd.it', 'Simone', 'Meneg
 insert into users
 (nickname, pwd, email, username, surname, usertype, ref)
 values ('JakeSun22', 'admin', 'nicolo.giaccone@studenti.unipd.it', 'Nicol&ograve;', 'Giaccone', 'admin', null);
+insert into users
+(nickname, pwd, email, username, surname, usertype, ref)
+values ('Enrico', 'admin', 'enrico.salmaso.2@studenti.unipd.it', 'Enrico', 'Salmaso', 'admin', null);
 
 
 
